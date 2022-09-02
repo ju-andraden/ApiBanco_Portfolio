c95926f9f558e57c1b2da9090c32cf08e1f30697
Juliana Andrade <juliana.andraden07@gmail.com>
2022-09-02 16:11:51 -0300

    ValidarEstadoAttributeTest ok

---
+++ Testes/Validacao/ValidarEstadoAttributeTest.cs
@@ -0,0 +1,26 @@
+﻿using Dominio.Validacao;
+using Xunit;
+
+namespace Testes.Validacao
+{
+    public class ValidarEstadoAttributeTest
+    {
+        private ValidarEstadoAttribute validarEstadoAttribute = new ValidarEstadoAttribute();
+
+        [Theory]
+        [InlineData("ab", false)]
+        [InlineData("12", false)]
+        [InlineData("#$", false)]
+        [InlineData("sp", true)]
+        [InlineData("SP", true)]
+        [InlineData("", true)]
+        [InlineData(null, true)]
+
+        public void IsValid(string value, bool result)
+        {
+            var estadoIsValid = validarEstadoAttribute.IsValid(value);
+
+            Assert.Equal(result, estadoIsValid);
+        }
+    }
+}
