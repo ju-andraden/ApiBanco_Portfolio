bb32ed8c3934dffffc65d10cc0269b222317d2d8
Juliana Andrade <juliana.andraden07@gmail.com>
2022-09-02 10:51:16 -0300

    ValidarCpfAttributeTest ok

---
+++ Testes/Validacao/ValidarCpfAttributeTest.cs
@@ -0,0 +1,24 @@
+﻿using Dominio.Validacao;
+using Xunit;
+
+namespace Testes.Validacao
+{
+    public class ValidarCpfAttributeTest
+    {
+        private ValidarCpfAttribute validarCpfAttribute = new ValidarCpfAttribute();
+
+        [Theory]
+        [InlineData("abc.def.ghi-jk", false)]
+        [InlineData("123.@56.789-10", false)]
+        [InlineData("123456.789-10", false)]
+        [InlineData("", false)]
+        [InlineData("123.456.789-10", true)]
+        [InlineData(null, true)]
+        public void IsValid(string value, bool result)
+        {
+            var cpfIsValid = validarCpfAttribute.IsValid(value);
+
+            Assert.Equal(result, cpfIsValid);
+        }
+    }
+}
