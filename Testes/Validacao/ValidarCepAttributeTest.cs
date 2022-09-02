807cc63f196427b968381ed82bb1b6fbf3b34f0d
Juliana Andrade <juliana.andraden07@gmail.com>
2022-09-02 10:39:05 -0300

    ValidarCepAttributeTest ok

---
+++ Testes/Validacao/ValidarCepAttributeTest.cs
@@ -0,0 +1,24 @@
+﻿using Dominio.Validacao;
+using Xunit;
+
+namespace Testes.Validacao
+{
+    public class ValidarCepAttributeTest
+    {
+        private ValidarCepAttribute validarCepAttribute = new ValidarCepAttribute();
+
+        [Theory]
+        [InlineData("abcd", false)]
+        [InlineData("@#$*", false)]
+        [InlineData("12345678", false)]
+        [InlineData("12345-678", true)]
+        [InlineData("", true)]
+        [InlineData(null, true)]
+        public void IsValid(string value, bool result)
+        {
+            var cepIsValid = validarCepAttribute.IsValid(value);
+
+            Assert.Equal(result, cepIsValid);
+        }
+    }
+}
