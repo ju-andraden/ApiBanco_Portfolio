5c73935a9a86f2ebeb7205e17fa90095317f67eb
Juliana Andrade <juliana.andraden07@gmail.com>
2022-09-03 16:36:42 -0300

    ValidarAgenciaAttributeTest ok

---
+++ Testes/Validacao/ValorMaiorQueZeroAttributeTest.cs
@@ -0,0 +1,21 @@
+﻿using Dominio.Validacao;
+using Xunit;
+
+namespace Testes.Validacao
+{
+    public class ValorMaiorQueZeroAttributeTest
+    {
+       private ValorMaiorQueZeroAttribute valorMaiorQueZeroAttribute = new ValorMaiorQueZeroAttribute();
+
+        [Theory]
+        [InlineData("-1", false)]
+        [InlineData("1", true)]
+
+        public void IsValid(string value, bool result)
+        {
+            var valorMaiorQueZeroIsValid = valorMaiorQueZeroAttribute.IsValid(value);
+
+            Assert.Equal(result, valorMaiorQueZeroIsValid);
+        }
+    }
+}
