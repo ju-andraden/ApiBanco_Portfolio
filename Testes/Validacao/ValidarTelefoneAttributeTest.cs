2f01cc57f39a3b3b65759679ab16746e63d70afa
Juliana Andrade <juliana.andraden07@gmail.com>
2022-09-03 16:32:25 -0300

    ValidarTelefoneAttributeTest ok

---
+++ Testes/Validacao/ValidarTelefoneAttributeTest.cs
@@ -0,0 +1,23 @@
+﻿using Dominio.Validacao;
+using Xunit;
+
+namespace Testes.Validacao
+{
+    public class ValidarTelefoneAttributeTest
+    {
+        private ValidarTelefoneAttribute validarTelefoneAttribute = new ValidarTelefoneAttribute();
+
+        [Theory]
+        [InlineData("(ab)cdef-ghij", false)]
+        [InlineData("(11)98504-5363", true)]
+        [InlineData("(11)8504-5363", true)]
+        [InlineData("", true)]
+        [InlineData(null, true)]
+        public void IsValid(string value, bool result)
+        {
+            var telefoneIsValid = validarTelefoneAttribute.IsValid(value);
+
+            Assert.Equal(result, telefoneIsValid);
+        }
+    }
+}
