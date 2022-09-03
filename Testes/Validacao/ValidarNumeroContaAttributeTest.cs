494abd736c6e6cadabf0a55a53496b384fe90dd9
Juliana Andrade <juliana.andraden07@gmail.com>
2022-09-03 16:22:36 -0300

    ValidarNumeroContaAttributeTest ok

---
+++ Testes/Validacao/ValidarNumeroContaAttributeTest.cs
@@ -0,0 +1,23 @@
+﻿using Dominio.Validacao;
+using Xunit;
+
+namespace Testes.Validacao
+{
+    public class ValidarNumeroContaAttributeTest
+    {
+        private ValidarNumeroContaAttribute validarNumeroContaAttribute = new ValidarNumeroContaAttribute();
+
+        [Theory]
+        [InlineData("abcde-f", false)]
+        [InlineData("@#$*!-@", false)]
+        [InlineData("12345-6", true)]
+        [InlineData("", true)]
+        [InlineData(null, true)]
+        public void IsValid(string value, bool result)
+        {
+            var numeroContaIsValid = validarNumeroContaAttribute.IsValid(value);
+
+            Assert.Equal(result, numeroContaIsValid);
+        }
+    }
+}
